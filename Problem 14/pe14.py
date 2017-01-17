d7c661bfcd99a28464d8b7a69ba398521cf5df58
Jeremiah Lantzer <jeremiahlantzer25@gmail.com>
2017-01-17 17:22:24 -0500

    Saving progress on problem 14

---
+++ Problem 14/pe14.py
@@ -0,0 +1,15 @@
+"""
+This program has been written to solve problem 14 on projecteuler.net
+author: Jeremiah Lantzer
+"""
+
+starting_number = 1000000
+dynamic_condition = 0
+chain = []
+
+while starting_number > dynamic_condition:
+    if starting_number % 2 == 0:
+        
+    else:
+
+
