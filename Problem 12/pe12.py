e84b9911489e5d61c8d0aa808cad3fe68934feda
Jeremiah Lantzer <jeremiahlantzer25@gmail.com>
2017-01-16 22:56:33 -0500

    It works quickly now

--- Problem 12/pe12.py
+++ Problem 12/pe12.py
@@ -3,8 +3,10 @@ This program has been written to solve problem 12 on projecteuler.net
 author: Jeremiah Lantzer
 """
 
-divisibles = 0
+import math
+
 counter = 0
+divisibles = 0
 
 
 def tri_number(amount):
@@ -14,13 +16,27 @@ def tri_number(amount):
     return triangle_number  # returns a certain triangle number given a positional number (1st, 2nd, 3rd)
 
 
-while divisibles < 500:
-    divisibles = 0
-    counter += 1
-    nat_sum = tri_number(counter)
-    for i in range(1, nat_sum+1):
+def gen_divs(nat_sum):
+    divisors = []
+    for i in range(1, int(nat_sum**0.5)+1):
         if nat_sum % i == 0:
-            divisibles += 1
-    print("Amount of divisors for ", nat_sum, ": ", divisibles)
+            yield i
+            if i is not nat_sum/i:
+                divisors.insert(0, nat_sum / i)
+    for div in divisors:
+        yield div
 
-print(nat_sum)
+def divs_actual(n):
+    return [div for div in gen_divs(n)]
+
+while divisibles < 500:
+    counter += 1
+    nums = tri_number(counter)
+    divs = divs_actual(nums)
+    divisibles = len(divs)
+
+print("Final: ", nums)
+print("Divisors: ", divisibles)
+
+# print("Amount of divisors for ", nat_sum, ": ", divisibles)
+
