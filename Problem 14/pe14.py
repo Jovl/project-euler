c695affced779fe25343d7464c8039e233d5abe5
Jeremiah Lantzer <jeremiahlantzer25@gmail.com>
2017-01-18 12:54:06 -0500

    saving progress

--- Problem 14/pe14.py
+++ Problem 14/pe14.py
@@ -3,13 +3,31 @@ This program has been written to solve problem 14 on projecteuler.net
 author: Jeremiah Lantzer
 """
 
+
+def even_formula(n):
+    return n / 2
+
+
+def odd_formula(n):
+    return 3 * n + 1
+
+
+def next_step(n):
+    if n % 2 == 0:
+        step = even_formula(n)
+        return next_step(step)
+    elif n == 1:
+        return 1
+    else:
+        step = odd_formula(n)
+        return next_step(step)
+
 starting_number = 1000000
 dynamic_condition = 0
+check = 0
 chain = []
 
 while starting_number > dynamic_condition:
-    if starting_number % 2 == 0:
-        
-    else:
-
-
+    starting_number -= 1
+    while check != 1:
+        check = next_step(starting_number)
