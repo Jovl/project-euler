8c11f9043c9f3ab8aa8c3f498b75c01abc6e9bf8
Jeremiah Lantzer <jeremiahlantzer25@gmail.com>
2017-01-20 12:51:47 -0500

    Saving progress. Grid genrating.

---
+++ Problem 15/pe15.py
@@ -0,0 +1,9 @@
+"""
+This program has been written to solve problem 15 on projecteuler.net
+author: Jeremiah Lantzer
+"""
+value = 3
+grid_points = 9
+plot_lines = int(grid_points / 2)
+grid = [['o' for x in range(1, plot_lines)] for y in range(1, plot_lines)]
+print(grid)
